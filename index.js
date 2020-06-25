725a244fb1ba698959bc296b8ac9a2439b3d6410
Lorna <lroberts1980@yahoo.com>
2020-06-25 20:45:11 +0100

    early basic functionality- all backend

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-document.getElementById("randomBtn").addEventListener("click", displayRandom);
+function getRandomBeer(){
 
-function displayRandom(){
-    console.log('random here')
+var beer = axios.get('https://api.punkapi.com/v2/beers/random')
+   .then(response =>{
+     let beerExample = response.data[0];
+     let beerName = beerExample.name;
+    let beerFood = beerExample.food_pairing;
+    console.log(beerName, beerFood);
+     return beerExample;
+   })
+   .catch(error => {
+    // handle error
+    console.log(error);
+   })
+
 }
+
+function getUserFood() {
+  let userChoice = document.getElementById('food').value;  
+  console.log(userChoice);
+}
+
+
+
+   /* function getMatchedFood(){
+
+        var list = axios.get('https://api.punkapi.com/v2/beers', {
+            params: {
+                food: `${userChoice}`
+            
+        }
+    })
+        .then(response =>{
+             console.log(response);
+           })
+           .catch(error => {
+            // handle error
+            console.log(error);
+           
+           })
+        }; */
